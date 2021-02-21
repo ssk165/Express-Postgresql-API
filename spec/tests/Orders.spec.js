16a748540aa15d42520de9a2eade705bf04f2a01
shubham16598 <shubham16598@gmail.com>
2021-02-21 23:16:43 +0530

    Order Spec

---
+++ spec/tests/Orders.spec.js
@@ -0,0 +1,23 @@
+const Request = require("request");
+
+describe("Order Test", () => {
+    let token = '';
+    const url = "http://0.0.0.0:3000";
+    beforeAll((done) => {
+        Request.get(`${url}/users/generatetoken`, (error, response, body) => {
+            token = JSON.parse(response.body).token;
+            done();
+        });
+    });
+    it("/order/id Test where id = userid", (done) => {
+        let status = 0;
+        console.log(token);
+        Request.get({url:`${url}/orders/2`,headers:{'x-auth-token':token}}, (error, response, body) => {
+            console.log(response.statusCode);
+            status = response.statusCode;
+            expect(status).toBe(200);
+            done();
+        });
+    });
+
+});
