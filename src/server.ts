0864503db738d440c35b23f09864fc256aa91d12
shubham16598 <shubham16598@gmail.com>
2021-02-21 17:25:25 +0530

    CORS Enable

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
 
 dotenv.config();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
